489b1d309458a1696b30af070e4b8bd394571c25
Pierre Béguet <pbeguet@adacore.com>
2023-04-17 07:42:52 +0000

    Fix imports discrepancy between static and shared ESALs
    Add IMPORTED_LIBRARIES .lexch information for static libraries.
    GPRlib now uses IMPORTED_LIBRARIES .lexch information to add
    partial linker -L/-l options for static libraries.
    
    Issue: eng/gpr/gprbuild#16
    
    Closes #16
    
    Merge request eng/gpr/gprbuild!32 from branch '16-fix-imports-discrepancy-between-static-and-shared-esals' into 'master'
    
    
    (cherry picked from commit 7f5493e91eaa28d3afc907034fb45d01819ff5d9)
    
    a625a811 Fix imports discrepancy between static and shared ESALs

--- src/gprbuild-post_compile.adb
+++ src/gprbuild-post_compile.adb
@@ -3295,9 +3295,6 @@ package body Gprbuild.Post_Compile is
                  (Exchange_File, Library_Label (Major_Minor_Id_Supported));
             end if;
 
-            Process_Imported_Libraries
-              (For_Project, There_Are_SALs => Disregard);
-
             --  Relocatable
 
             Put_Line (Exchange_File, Library_Label (Relocatable));
@@ -3319,8 +3316,6 @@ package body Gprbuild.Post_Compile is
             Write_Leading_Library_Options;
 
             Write_Library_Rpath_Options;
-
-            Write_Imported_Libraries;
          end if;
 
          Write_Runtime_Library_Dir;
@@ -3335,7 +3330,17 @@ package body Gprbuild.Post_Compile is
 
          Write_Toolchain_Version;
 
+         if not Is_Static (For_Project)
+           or else For_Project.Standalone_Library = Encapsulated
+         then
+            Process_Imported_Libraries
+              (For_Project, There_Are_SALs => Disregard);
+
+            Write_Imported_Libraries;
+         end if;
+
          if For_Project.Standalone_Library /= No then
+
             if For_Project.Lib_Auto_Init then
                Put_Line (Exchange_File, Library_Label (Auto_Init));
             end if;
@@ -4769,7 +4774,7 @@ package body Gprbuild.Post_Compile is
                Proj_List := Proj_List.Next;
             end loop;
 
-            if Main_Source.Unit = No_Unit_Index and then (not Dep_Files) then
+            if Main_Source.Unit = No_Unit_Index and then not Dep_Files then
                Close (Exchange_File);
 
                begin
